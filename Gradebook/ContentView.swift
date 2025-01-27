de5d60a659e329d2f9d47a9fadbcf7a15c05dcbb
Russell Gordon <rgordon@lcs.on.ca>
2025-01-27 10:36:27 -0500

    Initial Commit

---
+++ Gradebook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gradebook
+//
+//  Created by Russell Gordon on 2025-01-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gradebook/GradebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradebookApp.swift
+//  Gradebook
+//
+//  Created by Russell Gordon on 2025-01-27.
+//
+
+import SwiftUI
+
+@main
+struct GradebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
