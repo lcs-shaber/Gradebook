09881b111a11a694c4ebc1ae7600ed0f1924c4ac
Russell Gordon <rgordon@lcs.on.ca>
2025-01-27 10:44:23 -0500

    Created a landing view.

--- GradebookMobile/Views/LandingView.swift
+++ GradebookMobile/Views/LandingView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LandingView.swift
 //  GradebookMobile
 //
 //  Created by Russell Gordon on 2025-01-27.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandingView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    LandingView()
 }
